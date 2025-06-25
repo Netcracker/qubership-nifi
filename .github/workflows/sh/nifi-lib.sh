947c72228a6b504baed95c661c654bc20081cee9
Vladislav-Romanov27 <vladislav.romanov.qubership@gmail.com>
2025-06-25 13:24:44 +0300

    fix: fixed formatting errors in nifi-lib.sh

--- .github/workflows/sh/nifi-lib.sh
+++ .github/workflows/sh/nifi-lib.sh
@@ -281,15 +281,15 @@ create_global_vars_file() {
 
 generate_add_nifi_certs(){
     keytool -genkeypair -alias keycloakCA -keypass "$KEYCLOAK_TLS_PASS" -keystore ./temp-vol/tls-cert/keycloak.p12 -storetype PKCS12 \
-     -storepass "$KEYCLOAK_TLS_PASS" -keyalg RSA -dname "CN=keycloakCA" -ext bc:c
+        -storepass "$KEYCLOAK_TLS_PASS" -keyalg RSA -dname "CN=keycloakCA" -ext bc:c
     keytool -genkeypair -alias keycloakServer -keypass "$KEYCLOAK_TLS_PASS" -keystore ./temp-vol/tls-cert/keycloak.p12 -storetype PKCS12 \
-     -storepass "$KEYCLOAK_TLS_PASS" -keyalg RSA  -dname "CN=keycloak" -signer keycloakCA -signerkeypass \
-     "$KEYCLOAK_TLS_PASS" -ext SAN=dns:keycloak -ext SAN=dns:localhost
+        -storepass "$KEYCLOAK_TLS_PASS" -keyalg RSA  -dname "CN=keycloak" -signer keycloakCA -signerkeypass \
+        "$KEYCLOAK_TLS_PASS" -ext SAN=dns:keycloak -ext SAN=dns:localhost
     keytool -importkeystore -srckeystore ./temp-vol/tls-cert/keycloak.p12 -destkeystore ./temp-vol/tls-cert/keycloak-server.p12 -srcstoretype PKCS12 \
-     -deststoretype PKCS12 -srcstorepass "$KEYCLOAK_TLS_PASS" -deststorepass "$KEYCLOAK_TLS_PASS" -srcalias \
-     keycloakServer -destalias keycloakServer -srckeypass "$KEYCLOAK_TLS_PASS" -destkeypass "$KEYCLOAK_TLS_PASS"
+        -deststoretype PKCS12 -srcstorepass "$KEYCLOAK_TLS_PASS" -deststorepass "$KEYCLOAK_TLS_PASS" -srcalias \
+        keycloakServer -destalias keycloakServer -srckeypass "$KEYCLOAK_TLS_PASS" -destkeypass "$KEYCLOAK_TLS_PASS"
     keytool -exportcert -keystore ./temp-vol/tls-cert/keycloak.p12 -storetype PKCS12 -storepass "$KEYCLOAK_TLS_PASS" -alias keycloakCA -rfc \
-     -file ./temp-vol/tls-cert/ca/keycloak-ca.cer
+        -file ./temp-vol/tls-cert/ca/keycloak-ca.cer
     keytool -importcert -keystore ./temp-vol/tls-cert/keycloak-server.p12 -storetype PKCS12 -storepass "$KEYCLOAK_TLS_PASS" \
-     -file ./temp-vol/tls-cert/ca/keycloak-ca.cer -alias keycloak-ca-cer -noprompt
+        -file ./temp-vol/tls-cert/ca/keycloak-ca.cer -alias keycloak-ca-cer -noprompt
 }
