e29a601061aa920db258066b852d485a7b2a5a01
Dmitriy Myasnikov <myasnikov.qubership@gmail.com>
2026-04-02 14:34:14 +0300

    chore: remove unused imports

--- qubership-consul/qubership-consul-util/src/main/java/org/qubership/cloud/nifi/config/XmlConfigValidator.java
+++ qubership-consul/qubership-consul-util/src/main/java/org/qubership/cloud/nifi/config/XmlConfigValidator.java
@@ -17,8 +17,6 @@
 package org.qubership.cloud.nifi.config;
 
 import org.qubership.cloud.nifi.config.xml.BaseXmlConfigValidator;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
