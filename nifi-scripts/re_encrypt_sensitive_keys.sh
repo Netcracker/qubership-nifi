671e24a45909de6aa3b681848becdc00b5cbc5d4
Dmitriy Myasnikov <myasnikov.qubership@gmail.com>
2026-07-30 16:48:35 +0300

    feat: add configurable path for sensitive key variable

--- nifi-scripts/re_encrypt_sensitive_keys.sh
+++ nifi-scripts/re_encrypt_sensitive_keys.sh
@@ -19,15 +19,19 @@
 info "re_encrypt_sensitive_keys.sh start"
 ### fetch sensitive key from file
 if [[ -z "$NIFI_NEW_SENSITIVE_KEY" ]]; then
-    if [[ -f "/tmp/nifi-sens-key/key" ]]; then
-        info "Found sensitive key file /tmp/nifi-sens-key/key, fetching data"
-        NIFI_NEW_SENSITIVE_KEY=$(cat "/tmp/nifi-sens-key/key")
+    if [[ -n "$NIFI_SENSITIVE_KEY_PATH" ]]; then
+        if [[ -f "$NIFI_SENSITIVE_KEY_PATH" ]]; then
+            info "Found sensitive key file $NIFI_SENSITIVE_KEY_PATH, fetching data"
+            NIFI_NEW_SENSITIVE_KEY=$(cat "$NIFI_SENSITIVE_KEY_PATH")
+        else
+            warn "NIFI_NEW_SENSITIVE_KEY is not set and sensitive key file $NIFI_SENSITIVE_KEY_PATH does not exist"
+        fi
     else
-        warn "NIFI_NEW_SENSITIVE_KEY is not set and sensitive key file /tmp/nifi-sens-key/key does not exist"
+        warn "Neither NIFI_NEW_SENSITIVE_KEY, nor NIFI_SENSITIVE_KEY_PATH variable is set"
     fi
 fi
 if [ -z "${NIFI_NEW_SENSITIVE_KEY}" ] || [ "${NIFI_NEW_SENSITIVE_KEY}" = '<empty>' ] || [ "${NIFI_NEW_SENSITIVE_KEY}" = "" ]; then
-    error "Either NIFI_NEW_SENSITIVE_KEY env variable or /tmp/nifi-sens-key/key file should be non-empty. Terminating start-up..."
+    error "Either NIFI_NEW_SENSITIVE_KEY variable or file under path set in NIFI_SENSITIVE_KEY_PATH should be non-empty. Terminating start-up..."
     sleep 10
     exit 3
 fi
